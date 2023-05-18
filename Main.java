8f7669f40b65471b70c1f1aa0ccbb72073d5fcf8
NiletheDeal <122565235+NiletheDeal@users.noreply.github.com>
2023-05-18 12:18:07 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,35 @@
 package BlackJack;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	Boolean playing = true;
+	int cardsPlayed;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	//Play
+	public void Game() {
+		while (playing == true) {
+			Scanner sc = new Scanner(System.in);
+			String input = sc.nextLine();
+			if (input == "hit") {
+				
+			}
+			if (input == "stand") {
+				
+			}
+			else {
+				System.out.println("Please enter a valid command of hit, stand, or quit");
+			}
+		}
+	}
+	//Hit
+	//Stand
+	//Check Win
+	//Check Tie
 
 }
