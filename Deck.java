c96d3f38712734ce7c3ae6abdab79fce2b4317ad
NiletheDeal <122565235+NiletheDeal@users.noreply.github.com>
2023-06-07 08:52:22 -0700

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,3 +1,6 @@
+//This Java file finds the deck text file, shuffles the deck, and deals a card if asked
+//Neel Madala, Lukas Martinek
+//6/7/2023
 package BlackJack;
 
 import java.io.File;
@@ -10,12 +13,12 @@ public class Deck {
 	
 	public ArrayList<String> deck = new ArrayList<String>();
 	int cardsPlayed = 0;
-	public Deck() {
+	public Deck() {//Get deck and shuffle deck
 		getFile();
 		shuffleDeck();
 	}
 	
-	public void getFile() {
+	public void getFile() {//Finds the deck file
 		try {
 			Scanner fileScanner = new Scanner (new File("deck.txt"));
 			while (fileScanner.hasNextLine() == true) {
@@ -26,14 +29,15 @@ public class Deck {
 			System.exit(0);
 		}
 	}
-	public void shuffleDeck() {
+	public void shuffleDeck() {//shuffles the deck file
 		Collections.shuffle(deck);
 	}
-	public String getCard() {
-		if (cardsPlayed >= 51) {
+	public String getCard() {//gets a card
+		if (cardsPlayed >= 51) {//if you play through entire deck then shuffle and go again
 			shuffleDeck();
 			cardsPlayed = 0;
 		}
+		//Deals card and adds 1 to cardsPlayed
 		String card = deck.get(cardsPlayed);
 		cardsPlayed++;
 		return card;
